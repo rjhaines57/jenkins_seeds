ee6268f79c84ebced159703f555861b13e20e284
Rob Haines <rhaines@synopsys.com>
2018-04-26 00:52:07 +0100

    first commit

---
+++ openmrs.groovy
@@ -0,0 +1,36 @@
+job('openmrs') {
+    scm {
+        
+    }
+    triggers {
+    }
+    steps {
+        shell('''#!/bin/bash
+
+# Fixed configuration from the container. DO NOT CHANGE
+COV_HOME=/opt/coverity
+
+
+#configuration for this recipe
+PROJECT=openmrs
+STREAM=${PROJECT}
+REPO=https://github.com/openmrs/openmrs-core.git
+WAR=webapp/target/openmrs.war
+COMMIT=9f12d2f6c1c8ebbaa51c996cb209528d2110ab03
+BUILD_COMMAND="mvn install -Dmaven.test.skip=true"
+FS_CAPTURE_SEARCH="webapp/src"
+
+#Language/Compiler Selection
+#Choose from: java javascript python php gcc clang ruby swift scala vb
+#Use space separated list
+#e.g CONFIGURE="java php javascript"
+CONFIGURE="java javascript"
+
+. ${COV_HOME}/build_helper.sh
+''')
+    }
+ 
+}
+
+ queue('openmrs')
+

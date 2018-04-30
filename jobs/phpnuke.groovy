fc6b56b3a9c6dccb855e4079239cfd8217f2f46b
Rob Haines <rhaines@synopsys.com>
2018-04-30 21:42:20 +0100

    Adding phpnuke

---
+++ jobs/phpnuke.groovy
@@ -0,0 +1,41 @@
+job('phpnuke') {
+    scm {
+        
+    }
+    triggers {
+    }
+    steps {
+		copyArtifacts('seed-job') {
+            includePatterns('auto_triage/openmrs.csv')
+            buildSelector {
+                latestSuccessful(true)
+            }
+		}
+        shell('''#!/bin/bash
+
+# Fixed configuration from the container. DO NOT CHANGE
+COV_HOME=/opt/coverity
+
+
+#configuration for this recipe
+PROJECT=openmrs
+STREAM=${PROJECT}
+REPO=https://github.com/turnkeylinux-apps/phpnuke.git
+FS_CAPTURE_SEARCH="webapp/src"
+
+#Language/Compiler Selection
+#Choose from: java javascript python php gcc clang ruby swift scala vb
+#Use space separated list
+#e.g CONFIGURE="java php javascript"
+CONFIGURE="php"
+
+. ${COV_HOME}/build_helper.sh
+''')
+    
+	
+	}
+ 
+}
+
+ queue('phpnuke')
+

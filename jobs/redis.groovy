65f4c0bf39f0771262030fe6330f1fa7c8d39ac6
Rob Haines <rhaines@synopsys.com>
2018-05-28 01:05:36 +0100

    Add escape characters in redis.groovy

--- jobs/redis.groovy
+++ jobs/redis.groovy
@@ -20,23 +20,23 @@ node {
         }
         stage('Build (C++)') {
       
-            docker.image('cov-analysis:2018.03').withRun('--hostname ${BUILD_TAG} -v '+volumeId+':/opt/coverity/idirs -v coverity_tools:/opt/coverity --name wibble') { c ->
-                docker.image('gcc:5.5.0').inside('--hostname ${BUILD_TAG} -v '+volumeId+':/opt/coverity/idirs -v maven:/root/.m2 -v coverity_tools:/opt/coverity:ro') { 
+            docker.image('cov-analysis:2018.03').withRun('--hostname \${BUILD_TAG} -v '+volumeId+':/opt/coverity/idirs -v coverity_tools:/opt/coverity --name wibble') { c ->
+                docker.image('gcc:5.5.0').inside('--hostname \${BUILD_TAG} -v '+volumeId+':/opt/coverity/idirs -v maven:/root/.m2 -v coverity_tools:/opt/coverity:ro') { 
                     sh '/opt/coverity/analysis/bin/cov-configure --config /opt/coverity/idirs/coverity_config.xml --gcc'
                     sh '/opt/coverity/analysis/bin/cov-build --dir /opt/coverity/idirs/idir  --config /opt/coverity/idirs/coverity_config.xml make -j auto '            
                 }            
             }
         }
         stage('Analysis') {
-            docker.image('cov-analysis:2018.03').inside('--hostname ${BUILD_TAG} --mac-address 08:00:27:ee:25:b2 -v '+volumeId+':/opt/coverity/idirs') {
+            docker.image('cov-analysis:2018.03').inside('--hostname \${BUILD_TAG} --mac-address 08:00:27:ee:25:b2 -v '+volumeId+':/opt/coverity/idirs') {
                 sh '/opt/coverity/analysis/bin/cov-analyze --dir /opt/coverity/idirs/idir --trial'
             }
         }
         stage('Commit') {
            withCoverityEnv(coverityToolName: 'default', connectInstance: 'Test Server') { 
-                docker.image('cov-analysis:2018.03').inside('--network docker_coverity --hostname ${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v '+volumeId+':/opt/coverity/idirs -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=${COV_USER} -e COV_PASSPHRASE=${COV_PASSPHRASE}') {
-                    sh 'createProjectAndStream --host ${COVERITY_HOST} --user ${COV_USER} --password coverity --project Redis --stream redis'
-                    sh '/opt/coverity/analysis/bin/cov-commit-defects --dir /opt/coverity/idirs/idir --host ${COVERITY_HOST} --port ${COVERITY_PORT} --stream redis'
+                docker.image('cov-analysis:2018.03').inside('--network docker_coverity --hostname \${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v '+volumeId+':/opt/coverity/idirs -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=\${COV_USER} -e COV_PASSPHRASE=\${COV_PASSPHRASE}') {
+                    sh 'createProjectAndStream --host \${COVERITY_HOST} --user \${COV_USER} --password coverity --project Redis --stream redis'
+                    sh '/opt/coverity/analysis/bin/cov-commit-defects --dir /opt/coverity/idirs/idir --host \${COVERITY_HOST} --port \${COVERITY_PORT} --stream redis'
                 }
             }
         }

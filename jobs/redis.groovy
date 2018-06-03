de6a80f6d058c3ae9ac01ae96549a47580e7c52f
Rob Haines <rhaines@synopsys.com>
2018-06-03 17:03:18 +0100

    Modified Redis to use docker hub credentials

--- jobs/redis.groovy
+++ jobs/redis.groovy
@@ -3,16 +3,26 @@ pipelineJob('Redis') {
     cps {
       sandbox()
       script("""
-
 node {
-    def volumeId
+    // Set volume Name to \${BUILD_TAG} for each build gives new volume and therefore clean
+    // environment. Use \${JOB_NAME} for incremental builds
+    def volumeName='\${BUILD_TAG}'
     def analysis_image="clittlej/sig-emea-ses:analysis-2018.03"
     try {
+
+        stage('create project') {
+           withCoverityEnv(coverityToolName: 'default', connectInstance: 'Test Server') { 
+                docker.image(analysis_image).inside('--network docker_coverity --hostname \${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v '+volumeName+':/opt/coverity/ -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=\${COV_USER} -e COV_PASSPHRASE=\${COV_PASSPHRASE}') {
+                    sh '/opt/coverity/analysis/bin/cov-commit-defects --dir /opt/coverity/idirs/idir --host \${COVERITY_HOST} --port \${COVERITY_PORT} --stream redis'
+                }
+            }
+        }
+
     
         stage('Setup idir')
         {
-	   //	copyArtifacts filter: 'auto_triage/openmrs.csv', fingerprintArtifacts: true, projectName: 'seed-job', selector: lastSuccessful()    
-        deleteDir()    
+	    //	copyArtifacts filter: 'auto_triage/openmrs.csv', fingerprintArtifacts: true, projectName: 'seed-job', selector: lastSuccessful()    
+        //deleteDir()    
         volumeId=sh(returnStdout: true, script: '/usr/bin/docker volume create').trim() 
         }
         stage('Clone sources') {
@@ -20,24 +30,29 @@ node {
 		  'sh git checkout 070d04717909e25254334f55760e972c6f8d02e3'
         }
         stage('Build (C++)') {
-      
-            docker.image(analysis_image).withRun('--hostname \${BUILD_TAG} -v \${BUILD_TAG}:/opt/coverity') { 
-                docker.image('gcc:5.5.0').inside('--hostname \${BUILD_TAG} -v \${BUILD_TAG}:/opt/coverity') { 
-                    sh '/opt/coverity/analysis/bin/cov-configure --config /opt/coverity/idirs/coverity_config.xml --gcc'
-                    sh '/opt/coverity/analysis/bin/cov-build --dir /opt/coverity/idirs/idir --config /opt/coverity/idirs/coverity_config.xml make -j'            
-                    sh '/opt/coverity/analysis/bin/cov-import-scm --dir /opt/coverity/idirs/idir --scm git --filename-regex \${WORKSPACE}'
-                }            
+        docker.withRegistry('','docker_credentials') {
+           docker.image(analysis_image).withRun('--hostname \${BUILD_TAG} -v '+volumeName+':/opt/coverity') { 
+                    docker.image('gcc:5.5.0').inside('--hostname \${BUILD_TAG} -v '+volumeName+':/opt/coverity') { 
+                        sh '/opt/coverity/analysis/bin/cov-configure --config /opt/coverity/idirs/coverity_config.xml --gcc'
+                        sh '/opt/coverity/analysis/bin/cov-build --dir /opt/coverity/idirs/idir --config /opt/coverity/idirs/coverity_config.xml make -j'            
+                        try {
+                            sh '/opt/coverity/analysis/bin/cov-import-scm --dir /opt/coverity/idirs/idir --scm git --filename-regex \${WORKSPACE}'
+                        }
+                        catch (err)  { echo "cov-import-scm return something "}
+                        
+                    }            
+                }
             }
         }
 		
         stage('Analysis') {
-            docker.image(analysis_image).inside('--hostname \${BUILD_TAG} --mac-address 08:00:27:ee:25:b2 -v \${BUILD_TAG}:/opt/coverity') {
+            docker.image(analysis_image).inside('--hostname \${BUILD_TAG} --mac-address 08:00:27:ee:25:b2 -v '+volumeName+':/opt/coverity') {
                 sh '/opt/coverity/analysis/bin/cov-analyze --strip-path \${WORKSPACE} --dir /opt/coverity/idirs/idir --trial'
             }
         }
         stage('Commit') {
            withCoverityEnv(coverityToolName: 'default', connectInstance: 'Test Server') { 
-                docker.image(analysis_image).inside('--network docker_coverity --hostname \${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v \${BUILD_TAG}:/opt/coverity/ -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=\${COV_USER} -e COV_PASSPHRASE=\${COV_PASSPHRASE}') {
+                docker.image(analysis_image).inside('--network docker_coverity --hostname \${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v '+volumeName+':/opt/coverity/ -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=\${COV_USER} -e COV_PASSPHRASE=\${COV_PASSPHRASE}') {
                     sh 'python /opt/coverity/cov_tools/coverity_jenkins_tools/createProjectAndStream.py --host \${COVERITY_HOST} --user \${COV_USER} --password coverity --project Redis --stream redis'
                     sh '/opt/coverity/analysis/bin/cov-commit-defects --dir /opt/coverity/idirs/idir --host \${COVERITY_HOST} --port \${COVERITY_PORT} --stream redis'
                 }
@@ -45,20 +60,16 @@ node {
         }
     }
     catch (err){
-      echo "Found error"
+      echo "Found error"+err
     }
     finally
     {
     stage('cleanup volume') {
         // Comment out this to keep the idir volume
-		sh 'docker volume rm '+volumeId+' \${BUILD_TAG}'
+		sh 'docker volume rm '+volumeName
     }
     }
-}
-      
-	  
-      
-	  """.stripIndent())      
+}	  """.stripIndent())      
     }
   }
 }

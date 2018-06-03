07c5a35aef2393adf6fb4db046dc71d0785664e5
Rob Haines <rhaines@synopsys.com>
2018-06-03 19:34:46 +0100

    More updates to redis

--- jobs/redis.groovy
+++ jobs/redis.groovy
@@ -9,24 +9,9 @@ node {
     def volumeName='\${BUILD_TAG}'
     def analysis_image="clittlej/sig-emea-ses:analysis-2018.03"
     try {
-
-        stage('create project') {
-           withCoverityEnv(coverityToolName: 'default', connectInstance: 'Test Server') { 
-                docker.image(analysis_image).inside('--network docker_coverity --hostname \${BUILD_TAG}  --mac-address 08:00:27:ee:25:b2 -v '+volumeName+':/opt/coverity/ -e HOME=/opt/coverity/idirs -w /opt/coverity/idirs -e COV_USER=\${COV_USER} -e COV_PASSPHRASE=\${COV_PASSPHRASE}') {
-                    sh '/opt/coverity/analysis/bin/cov-commit-defects --dir /opt/coverity/idirs/idir --host \${COVERITY_HOST} --port \${COVERITY_PORT} --stream redis'
-                }
-            }
-        }
-
-    
-        stage('Setup idir')
-        {
-	    //	copyArtifacts filter: 'auto_triage/openmrs.csv', fingerprintArtifacts: true, projectName: 'seed-job', selector: lastSuccessful()    
-        //deleteDir()    
-        volumeId=sh(returnStdout: true, script: '/usr/bin/docker volume create').trim() 
-        }
         stage('Clone sources') {
-          git branch: 'unstable', url: 'https://github.com/antirez/redis.git'
+			//deleteDir()    
+			git branch: 'unstable', url: 'https://github.com/antirez/redis.git'
 		  'sh git checkout 070d04717909e25254334f55760e972c6f8d02e3'
         }
         stage('Build (C++)') {
